57770fea98082dee7d00d20c079de66ab2dffec7
danushan99 <danuthasan08@gmail.com>
2021-09-29 21:30:24 +0530

    green btn

--- lib/main.dart
+++ lib/main.dart
@@ -69,18 +69,41 @@ class _HomePageState extends State<HomePage> {
                     });
                   },
                   icon: _isVisible
-                      ? Icon(Icons.visibility)
-                      : Icon(Icons.visibility_off),
+                      ? Icon(
+                          Icons.visibility,
+                          color: Colors.black,
+                        )
+                      : Icon(Icons.visibility_off, color: Colors.grey),
                 ),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                   borderSide: const BorderSide(color: Colors.black),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black),
                 ),
                 hintText: "Password",
                 contentPadding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
             ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(microseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
