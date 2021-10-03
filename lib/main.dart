9173d9e668716bbec45bffded96d0d024301d0ac
danushan99 <danuthasan08@gmail.com>
2021-10-03 23:37:05 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -43,144 +43,145 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Create Your Account",
-            style: const TextStyle(color: Colors.black),
-          ),
-          elevation: 0,
-          backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          "Create Your Account",
+          style: TextStyle(color: Colors.black),
         ),
-        body: Container(
-          color: Colors.white,
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Set a Password",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Please create a secure password including the following crieria below ",
-                style: TextStyle(
-                    fontSize: 16, height: 1.5, color: Colors.grey.shade600),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              TextField(
-                onChanged: (password) => onPasswordChanged(password),
-                obscureText: !_isVisible,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _isVisible = !_isVisible;
-                      });
-                    },
-                    icon: _isVisible
-                        ? const Icon(
-                            Icons.visibility,
-                            color: Colors.black,
-                          )
-                        : Icon(Icons.visibility_off, color: Colors.grey),
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.black),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(color: Colors.black),
-                  ),
-                  hintText: "Password",
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Set a Password",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Please create a secure password including the following crieria below ",
+              style: TextStyle(
+                  fontSize: 16, height: 1.5, color: Colors.grey.shade600),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              onChanged: (password) => onPasswordChanged(password),
+              obscureText: !_isVisible,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isVisible = !_isVisible;
+                    });
+                  },
+                  icon: _isVisible
+                      ? const Icon(
+                          Icons.visibility,
+                          color: Colors.black,
+                        )
+                      : const Icon(Icons.visibility_off, color: Colors.grey),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  AnimatedContainer(
-                    duration: const Duration(microseconds: 500),
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: _isPasswordEightCharacter
-                          ? Colors.green
-                          : Colors.transparent,
-                      border: _isPasswordEightCharacter
-                          ? Border.all(color: Colors.transparent)
-                          : Border.all(color: Colors.grey.shade400),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.check,
-                        color: Colors.white,
-                        size: 15,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text("Contains at least 8 characters"),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  AnimatedContainer(
-                    duration: Duration(microseconds: 500),
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: _ishasPasswordNumber
-                          ? Colors.green
-                          : Colors.transparent,
-                      border: Border.all(color: Colors.grey.shade400),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Center(
-                      child: Icon(
-                        Icons.check,
-                        color: Colors.white,
-                        size: 15,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text("Contains at least 1 Numbers"),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              MaterialButton(
-                height: 40,
-                minWidth: double.infinity,
-                color: Colors.black,
-                child: Text(
-                  "CREATE ACCOUNT",
-                  style: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.black),
                 ),
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+                hintText: "Password",
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               ),
-            ],
-          ),
-        ));
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(microseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: _isPasswordEightCharacter
+                        ? Colors.green
+                        : Colors.transparent,
+                    border: _isPasswordEightCharacter
+                        ? Border.all(color: Colors.transparent)
+                        : Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text("Contains at least 8 characters"),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(microseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: _ishasPasswordNumber
+                        ? Colors.green[400]
+                        : Colors.transparent,
+                    border: Border.all(color: Colors.grey.shade400),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Text("Contains at least 1 Numbers"),
+              ],
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            MaterialButton(
+              height: 40,
+              minWidth: double.infinity,
+              color: Colors.black,
+              child: const Text(
+                "CREATE ACCOUNT",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
